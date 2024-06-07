eed80018527e472afe703eee6a0c7a2cd70281e6
Tanmay Singh Thakur <thakurtanmaysingh@gmail.com>
2024-06-07 17:50:12 +0530

    display countries

--- country-flags/src/App.js
+++ country-flags/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerStyle">
+      {countries.map((country) => (
+        <div key={country.cca3} className="cardStyle">
+          <img
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            className="imageStyle"
+          />
+          <h2>{country.name.common}</h2>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
